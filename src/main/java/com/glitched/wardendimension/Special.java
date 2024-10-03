0b7a2747757f0dd1dc6f118d58aa1f6efd0b9902
GlitchCode24 <georgepap5671@gmail.com>
2024-10-03 16:17:49 +0300

    Complete 1.0.0

--- src/main/java/com/glitched/wardendimension/Special.java
+++ src/main/java/com/glitched/wardendimension/Special.java
@@ -1,14 +1,22 @@
 package com.glitched.wardendimension;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
 
 public class Special {
     public static void init() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+            content.add(ModItems.WARDEN_CORE);
+        });
         LootTableEvents.MODIFY.register((key, tableBuilder, source,registries) -> {
             if(source.isBuiltin() && (EntityType.WARDEN.getLootTableId().equals(key))) {
                 LootPool.Builder poolBuilder = LootPool.builder()
@@ -17,5 +25,11 @@ public class Special {
                 tableBuilder.pool(poolBuilder);
             }
         });
+        CustomPortalBuilder.beginPortal()
+                .frameBlock(Blocks.REINFORCED_DEEPSLATE)
+                .lightWithItem(ModItems.WARDEN_CORE)
+                .destDimID(Identifier.of(WardenDimension.MOD_ID,"warden"))
+                .tintColor(22,127,165)
+                .registerPortal();
     }
 }
